f817b939e58d631e6a625a535669d7bbe2fa04f7
Lena Bartell <lbartell@morsecorp.com>
2020-10-27 19:53:26 -0400

    configure logging

--- whatsappnalysis/__init__.py
+++ whatsappnalysis/__init__.py
@@ -1,4 +1,14 @@
+import datetime
+from pathlib import Path
+
 import nltk
+from loguru import logger
+
 
 nltk.download("vader_lexicon")
 nltk.download("punkt")
+
+# Configure loguru
+log_file_name = f"log_{datetime.datetime.now().isoformat(sep='_')}.log"
+log_file_path = Path(__file__).parent / log_file_name
+logger.add(log_file_path.open('w'))
