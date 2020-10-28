de669b60adf43990ddaf16fe6622f0bec9169d47
Lena Bartell <lbartell@morsecorp.com>
2020-10-27 20:08:23 -0400

    configure logging

--- whatsappnalysis/__init__.py
+++ whatsappnalysis/__init__.py
@@ -12,4 +12,6 @@ nltk.download("punkt")
 # Configure loguru
 log_file_name = f"log_{datetime.datetime.now().isoformat(sep='_')}.log"
 log_file_path = Path(__file__).parent.parent / log_file_name
+logger.add(sys.stderr)
+logger.add(sys.stdout)
 logger.add(log_file_path, rotation="500 MB")
